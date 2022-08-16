3470300c70b68e178ab88c3c2382d6126d2fce01
zaina <tahazena20@gmail.com>
2022-08-16 15:16:11 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+export interface UserVm {
+  name: string;
+  age: string;
+  email:string;
+  password:string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
-}
+  public userList: UserVm[]=[{
+    name:'zaina',
+    age: '22',
+    email:"tahazena32@gmail.com",
+     password:'12324'
+  }
+  ];
+
+  public newUserVm: UserVm ={name:'',age:'',email:'',password:''};
+
+
+
+  public addUser(form:NgForm){
+
+    console.log(form.form.valid,this.newUserVm)
+    if(!form.form.valid){
+    form.form.markAllAsTouched();}
+    else{
+    this.userList.push(this.newUserVm)}
+   
+
+  }
+
+  deleteUser(user:UserVm){
+    let i=this.userList.indexOf(user)
+    if(i>=0){
+      this.userList.splice(i,1);
+    }
+    
+  }
+  }
+
+
+
+
+
+
+
